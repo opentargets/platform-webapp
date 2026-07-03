e442758b5908768063966977e98816f4f56832fa
Carlos Cruz <me@carcruzcast.com>
2026-07-03 15:39:39 +0100

    [Platform]: Update PATHWAYS_API_BASE_URL to use a new development endpoint

--- apps/platform/src/components/GeneEnrichmentAnalysis/config.ts
+++ apps/platform/src/components/GeneEnrichmentAnalysis/config.ts
@@ -1,3 +1,4 @@
 import { getConfig } from "@ot/config";
 
-export const PATHWAYS_API_BASE_URL = getConfig().urlPathwaysApi;
+// export const PATHWAYS_API_BASE_URL = getConfig().urlPathwaysApi;
+export const PATHWAYS_API_BASE_URL = "https://pathways-api-dev-426265110888.europe-west1.run.app/";
